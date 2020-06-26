827ee5c183db280dc8eeff58c3497a12cc0bc767
rodrigo-reynoso <rodrigosreynoso33@gmail.com>
2020-06-26 00:15:54 -0300

    agregue todas las modif en bifurcacion

--- app.js
+++ app.js
@@ -1,53 +1,78 @@
+// agregue formato con las reglas de prettier- Code formatter...fui a settings y luego di tilde a format on save para que me formate automaticamente cuando guardo
+
 var fecha = new Date(); // objeto fecha actual // var porque es la unica que es modificable dentro de una function
 
- var numDiasSemana = fecha.getDay();
- console.log(numDiasSemana);
-function textodia(fecha,numDiasSemana){
-    
-    var diasDeSemana = new Array('Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado');
-    var diasLetras =  diasDeSemana[fecha.getDay()]; // llamo a fecha
-    
-      
-    
-    return diasLetras;
- } 
- console.log(textodia(fecha));
- let darleColor = document.getElementById('diaSemana');
-     darleColor.style.color = 'rgb(13, 23, 173)'; 
- if(numDiasSemana % 2 == 0){    // le di color cuando es un dia par
-   let colorDinamico = document.getElementById('diaSemana');
-    console.log('hola esta mostrado en consola');
-    colorDinamico.style.color = '#f37121';
-   }
- 
- 
+var numDiasSemana = fecha.getDay();
+console.log(numDiasSemana);
+function textodia(fecha) {
+  var diasDeSemana = new Array(
+    "Domingo",
+    "Lunes",
+    "Martes",
+    "Miércoles",
+    "Jueves",
+    "Viernes",
+    "Sábado"
+  );
+  var diasLetras = diasDeSemana[fecha.getDay()]; // llamo a fecha
 
-    function textoMes(fecha){
-    var Mes = new Array( 'Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre');
-    var mesLetra = Mes[fecha.getMonth()];
-    
-    return mesLetra;
-    }
-    console.log(textoMes(fecha));
-    if(fecha.getMonth() % 2 !==0){  // le di color cuando es un mes impar
-       console.log('No es par este mes');
-       let colorDinamicoMes = document.getElementById('mesLetra');
-       colorDinamicoMes.style.color = '#111d5e';
-       
-    }
-    
-    var diaMes = (fecha.getDate());
-    console.log(diaMes);
-    if(diaMes % 2 !==0 ){ // le di color cuando es un dia impar
-      let colorDinamicoDiaMes = document.getElementById('dia');
-       console.log('hola,otra vez yo');
-       colorDinamicoDiaMes.style.color = '#ffbd69';
-      }
-    // document.getElementById('diaSemana').innerHTML = textodia(fecha);
-    // console.log(textodia(fecha));
-     document.getElementById('diaSemana').innerHTML = textodia(fecha) ;
-    
-    console.log(dia);
-    document.getElementById('dia').innerHTML = diaMes;
-    document.getElementById('mesLetra').innerHTML = textoMes(fecha);
-    console.log(mesLetra);
+  return diasLetras;
+}
+console.log(textodia(fecha));
+
+function diaSemanaPar(fecha) {
+  return fecha.getDay() % 2 == 0;
+}
+if (diaSemanaPar(fecha)) {
+  const colorDiaSemanaPar = document.getElementById("diaSemana");
+  console.log("dandole color al dia de la semana");
+  colorDiaSemanaPar.style.color = "#12cad6";
+}
+
+function textoMes(fecha) {
+  var Mes = new Array(
+    "Enero",
+    "Febrero",
+    "Marzo",
+    "Abril",
+    "Mayo",
+    "Junio",
+    "Julio",
+    "Agosto",
+    "Septiembre",
+    "Octubre",
+    "Noviembre",
+    "Diciembre"
+  );
+  var mesLetra = Mes[fecha.getMonth()];
+
+  return mesLetra;
+}
+console.log(textoMes(fecha));
+
+function esMesImpar(fecha) {
+  return fecha.getMonth() % 2 !== 0;
+}
+if (esMesImpar(fecha)) {
+  console.log("dandole color al mes");
+  const colorMesImpar = document.getElementById("mesLetra");
+  colorMesImpar.style.color = "#f37121";
+}
+
+function diaMesImpar(fecha) {
+  return fecha.getDate() % 2 !== 0;
+}
+if (diaMesImpar(fecha)) {
+  const colorDiaMesImpar = document.getElementById("dia");
+  console.log("dandole color al dia");
+  colorDiaMesImpar.style.color = "#ffbd69";
+}
+
+ 
+// document.getElementById('diaSemana').innerHTML = textodia(fecha);
+// console.log(textodia(fecha));
+document.getElementById("diaSemana").innerHTML = textodia(fecha);
+document.getElementById("dia").innerHTML = fecha.getDate();
+document.getElementById("mesLetra").innerHTML = textoMes(fecha);
+console.log(dia);
+console.log(mesLetra);
